cbb881c80f560a4bbbbba47126f680f228393322
Lena <lenamariemue@gmail.com>
2022-01-13 15:02:08 +0100

    Change colorbar of rain depths.

--- code/colorbar.py
+++ code/colorbar.py
@@ -2,7 +2,7 @@ from matplotlib.colors import LinearSegmentedColormap
 
 
 # define colors (list of RGB tuples: left blue, right red)
-colors = [(0,0.19,1), (0,0.69,1), (0.44,1,0.56), (1,1,0), (1,0.69,0), (1,0,0), (0.5,0,0)]
+colors = [(1,1,1,0), (0,0.19,1), (0,0.69,1), (0.44,1,0.56), (1,1,0), (1,0.69,0), (1,0,0), (0.5,0,0)]
 
 # Create colormap with 150 bins: few bins > discrete colorbar
-cm = LinearSegmentedColormap.from_list("new_jet", colors, N=150) 
+cm = LinearSegmentedColormap.from_list("new_jet", colors, N=100) 
