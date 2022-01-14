097b7fded257dab220a7c2833c5d8f1ada0c50ea
Lena <lenamariemue@gmail.com>
2022-01-15 00:06:59 +0100

    Create composite of multiple radar sites. Georeferencing needs more time and love.

--- code/composite.py
+++ code/composite.py
@@ -1,3 +1,4 @@
+from re import A
 import sys
 import datetime
 import numpy as np
@@ -5,7 +6,6 @@ import matplotlib.pylab as pl
 import matplotlib.pyplot as plt
 import wradlib as wrl
 from wradlib.georef.projection import reproject
-import cartopy.crs as ccrs
 
 from colorbar import cm
 from func import plot_raindepths, plot_gridded, clutter_gabella, attcorr,  rain_depths
@@ -23,16 +23,9 @@ site_loc_umd = (11.176091, 52.160096, 185)
 site_loc_neu = (11.135034, 50.500114, 880)
 site_loc_eis = (12.402788, 49.540667, 799)
 
-# get domain width and heigth, 135km 
-lon_min = 11.135034
-lon_max = 13.858212
-lat_min = 49.540667
-lat_max = 52.648667
-
-# Calculate rain depths as (360,128) - array.
 def get_depths(filename):
     """ 
-    Read data from file. Correct clutter and attenuation. Calculate and return 5min - rain depths.
+    Read data from file. Correct clutter and attenuation. Calculate and return 5min - rain depths as (360,128) - array.
     """
     f = wrl.util.get_wradlib_data_file('example_data/'+filename)
     data, metadata = wrl.io.read_dx(f)
@@ -47,56 +40,76 @@ depths_pro = get_depths(filename_pro)
 depths_umd = get_depths(filename_umd)
 depths_neu = get_depths(filename_neu)
 depths_eis = get_depths(filename_eis)
-np.savetxt("code/depths_drs.txt", depths_drs)
 
 # Gridding
-def get_radolan_coords(depths, radar_loc):
+def get_coords(depths, radar_loc):
     # Get cartesian coordinates in xyz-space.
     elevation = 0.5 # in degree
     azimuths = np.arange(0,360)
     ranges = np.arange(0, 128000., 1000.)
     polargrid = np.meshgrid(ranges, azimuths)
     coords, rad = wrl.georef.spherical_to_xyz(polargrid[0], polargrid[1], elevation, radar_loc)
-
-    # Get geo coordinates.
-    utm_coords = wrl.georef.reproject(coords, projection_source=rad, projection_target=wrl.georef.epsg_to_osr(32632))
-    x = utm_coords[..., 0]
-    y = utm_coords[..., 1]
-    # radolan = wrl.georef.create_osr("dwd-radolan") # polarstereografischer Projektion, äquidistante Rasterung von 1,0 km
-    # radolan_coords = wrl.georef.reproject(coords, projection_source=rad, projection_target=radolan)
-    # x = radolan_coords[..., 0]
-    # y = radolan_coords[..., 1]
+    x = coords[..., 0]
+    y = coords[..., 1]
 
     # Gridding -> 2D array
-    xgrid = np.linspace(x.min(), x.max(), 250)
-    ygrid = np.linspace(y.min(), y.max(), 250)
+    xgrid = np.linspace(x.min(), x.max(), 256)
+    ygrid = np.linspace(y.min(), y.max(), 256)
     grid_xy = np.meshgrid(xgrid, ygrid)
     grid_xy = np.vstack((grid_xy[0].ravel(), grid_xy[1].ravel())).transpose()
     xy=np.concatenate([x.ravel()[:,None],y.ravel()[:,None]], axis=1)
-    gridded = wrl.comp.togrid(xy, grid_xy, 128000., np.array([x.mean(), y.mean()]), depths.ravel(), wrl.ipol.Idw)
-    gridded = np.ma.masked_invalid(gridded).reshape((len(xgrid), len(ygrid)))
+    gridded = wrl.comp.togrid(xy, grid_xy, 128000., np.array([0,0]), depths.ravel(), wrl.ipol.Idw)
+    # gridded = np.ma.masked_invalid(gridded).reshape((len(xgrid), len(ygrid)))
+    gridded = gridded.reshape((len(xgrid), len(ygrid)))
     return xgrid, ygrid, gridded
 
-xgrid_drs, ygrid_drs, gridded_drs = get_radolan_coords(depths_drs, site_loc_drs)
-xgrid_eis, ygrid_eis, gridded_eis = get_radolan_coords(depths_eis, site_loc_eis)
-xgrid_pro, ygrid_pro, gridded_pro = get_radolan_coords(depths_pro, site_loc_pro)
-xgrid_umd, ygrid_umd, gridded_umd = get_radolan_coords(depths_umd, site_loc_umd)
-xgrid_neu, ygrid_neu, gridded_neu = get_radolan_coords(depths_neu, site_loc_neu)
-np.savetxt("code/gridded_drs.txt", gridded_drs)
+xgrid_drs, ygrid_drs, gridded_drs = get_coords(depths_drs, site_loc_drs)
+xgrid_eis, ygrid_eis, gridded_eis = get_coords(depths_eis, site_loc_eis)
+xgrid_pro, ygrid_pro, gridded_pro = get_coords(depths_pro, site_loc_pro)
+xgrid_umd, ygrid_umd, gridded_umd = get_coords(depths_umd, site_loc_umd)
+xgrid_neu, ygrid_neu, gridded_neu = get_coords(depths_neu, site_loc_neu)
+
+
+# Calculate offset of sites pro, umd, eis, neu from site drs.
+xgrid_pro = [x+6*1000 for x in xgrid_drs]
+ygrid_pro = [y+169*1000 for y in ygrid_drs]
+xgrid_eis = [x-95*1000 for x in xgrid_drs]
+ygrid_eis = [y-176*1000 for y in ygrid_drs]
+xgrid_neu = [x-184*1000 for x in xgrid_drs]
+ygrid_neu = [y-69*1000 for y in ygrid_drs]
+xgrid_umd = [x-181*1000 for x in xgrid_drs]
+ygrid_umd = [y+69*1000 for y in ygrid_drs]
+
+# Add data to domain
+domain = np.zeros((700,550))
+def radar_into_domain(gridded_data, x_offset, y_offset):
+    """ 
+    Change nan's to zeros. Choose element wise maximum and insert in main domain.
+    """
+    x_offset-= 128
+    y_offset-= 128
+    domain_new = np.maximum(domain[y_offset:y_offset+gridded_data.shape[0], x_offset:x_offset+gridded_data.shape[1]], np.nan_to_num(gridded_data))
+    domain[y_offset:y_offset+gridded_data.shape[0], x_offset:x_offset+gridded_data.shape[1]] = domain_new
+    
+
+radar_into_domain(gridded_drs, 350, 350)
+radar_into_domain(gridded_eis, 255, 174)
+radar_into_domain(gridded_pro, 356, 519)
+radar_into_domain(gridded_umd, 168, 419)
+radar_into_domain(gridded_neu, 166 ,281)
 
 # Plotting
 fig = pl.figure(figsize=(10,8))
 ax = pl.subplot(111, aspect="equal")
-# ax = pl.axes(projection=ccrs.PlateCarree())
-# ax.coastlines()
-# ax.gridlines(draw_labels=True)
-pm1 = pl.pcolormesh(xgrid_drs, ygrid_drs, gridded_drs, cmap=cm)
-pm2 = pl.pcolormesh(xgrid_eis, ygrid_eis, gridded_eis, cmap=cm)
-pm3 = pl.pcolormesh(xgrid_pro, ygrid_pro, gridded_pro, cmap=cm)
-pm4 = pl.pcolormesh(xgrid_umd, ygrid_umd, gridded_umd, cmap=cm)
-pm4 = pl.pcolormesh(xgrid_neu, ygrid_neu, gridded_neu, cmap=cm)
-pl.colorbar()
+plt.imshow(domain, cmap=cm)
+cbar = pl.colorbar()
+cbar.ax.tick_params(labelsize=15) 
+cbar.set_label("5 min - Rain depths (mm)", fontsize=15)
+pl.xlim([0, 550])
+pl.ylim([0, 700])
+# pl.xlim([-350000, 200000])
+# pl.ylim([-350000, 350000])
 pl.xlabel("Easting (m)")
 pl.ylabel("Northing (m)")
-pl.title("UTM Zone 33")
+pl.title("Centered at DWD RADAR 10488 Dresden", fontsize=12)
 pl.savefig(f"images/composite_{filename_drs[15:25]}", dpi=600)
