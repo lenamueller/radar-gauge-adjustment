f16bec0cd57123e2f1f8326dc4b111a48f405dc9
Lena <lenamariemue@gmail.com>
2022-01-19 22:02:27 +0100

    Add cartopy and pint_xarray to create_venv.sh.

--- create_venv.sh
+++ create_venv.sh
@@ -3,6 +3,7 @@ python3 -m venv myenv
 source myenv/bin/activate
 
 pip3 install pip==21.3.1
+pip3 install cartopy=0.20.2
 pip3 install DateTime==4.3
 pip3 install matplotlib==3.5.1
 pip3 install netCDF4==1.5.8
@@ -10,5 +11,6 @@ pip3 install numpy==1.22.0
 pip3 install pandas==1.3.5
 pip3 install scipy==1.7.3
 pip3 install wradlib==1.13.0
+pip3 install pint_xarray
 
 deactivate
