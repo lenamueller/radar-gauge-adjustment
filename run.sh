eceb34e479c4992a368e03a840e1920d35da4dcb
Lena <lenamariemue@gmail.com>
2022-01-22 00:03:48 +0100

    Add hourly data download to run.sh.

--- run.sh
+++ run.sh
@@ -10,6 +10,8 @@ source myenv/bin/activate
 wget -r --level=1 https://opendata.dwd.de/weather/radar/sites/dx/drs/
 wget -r --level=1 https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/1_minute/precipitation/now/
 wget -r --level=1 https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/1_minute/precipitation/historical/2019/1minutenwerte_nieder_01048_20190101_20190131_hist.zip
+wget -r --level=1 https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/hourly/precipitation/historical/
+
 # set data path for wradlib
 export WRADLIB_DATA=/home/lena/Documents/projects/radar-gauge-adjustment/opendata.dwd.de/
 
