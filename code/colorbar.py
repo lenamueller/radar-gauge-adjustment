11fd7bb07cd9d9fe79e41bb6f08da18b5b41d4f3
Lena <lenamariemue@gmail.com>
2022-01-23 22:33:48 +0100

    Change colorbar to 45 steps.

--- code/colorbar.py
+++ code/colorbar.py
@@ -6,5 +6,5 @@ colors = [(1,1,1,0), (0,0.19,1), (0,0.69,1), (0.44,1,0.56), (1,1,0), (1,0.69,0),
 colors2 = [(1,1,1,0), (1,0,0)]
 
 # Create colormap with 150 bins: few bins > discrete colorbar
-cm = LinearSegmentedColormap.from_list("new_jet", colors, N=40) 
+cm = LinearSegmentedColormap.from_list("new_jet", colors, N=45) 
 cm_binary = LinearSegmentedColormap.from_list("new_jet", colors2, N=2) 
