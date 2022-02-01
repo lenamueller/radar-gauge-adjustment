bdcf1d4731af1841d0c1d7ab1b740ccb5a6e6682
Lena <lenamariemue@gmail.com>
2022-02-01 16:51:16 +0100

    Update latest precipitation radar.

--- run_20190109.sh
+++ run_20190109.sh
@@ -10,17 +10,17 @@ source myenv/bin/activate
 export WRADLIB_DATA=/home/lena/Documents/projects/radar-gauge-adjustment/opendata.dwd.de/
 
 # Download data from DWD`s open data server.
-# wget -q -r --level=1 https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/1_minute/precipitation/historical/2019/
-# wget -q -r --level=1 https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/hourly/precipitation/historical/
+wget -q -r --level=1 https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/1_minute/precipitation/historical/2019/
+wget -q -r --level=1 https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/hourly/precipitation/historical/
 
 #!/bin/sh
 # Run radar postprocessing and plotting. Arguments is time in minutes. Code works for 5 and 60 minutes.
-# python code/PlotCorrection.py "raa00-dx_10488-1901091200-drs---bin" 60
-# python code/PlotCorrection.py "raa00-dx_10392-1901091200-pro---bin" 60
-# python code/PlotCorrection.py "raa00-dx_10356-1901091200-umd---bin" 60
-# python code/PlotCorrection.py "raa00-dx_10557-1901091200-neu---bin" 60
-# python code/PlotCorrection.py "raa00-dx_10780-1901091200-eis---bin" 60
-python code/PlotComposite.py 5
+python code/PlotCorrection.py "raa00-dx_10488-1901091200-drs---bin" 60
+python code/PlotCorrection.py "raa00-dx_10392-1901091200-pro---bin" 60
+python code/PlotCorrection.py "raa00-dx_10356-1901091200-umd---bin" 60
+python code/PlotCorrection.py "raa00-dx_10557-1901091200-neu---bin" 60
+python code/PlotCorrection.py "raa00-dx_10780-1901091200-eis---bin" 60
+python code/PlotComposite.py 60
 
 # Deactivate virtual env.
 deactivate
